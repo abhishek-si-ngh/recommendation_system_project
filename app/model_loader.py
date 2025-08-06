55cc0126bc2d9bcedfc86f89e9c80114aeb4c8bc
Abhishek Singh <asdec0302@gmail.com>
2025-08-06 21:52:34 +0530

    Fix paths and Python version for Render

--- app/model_loader.py
+++ app/model_loader.py
@@ -2,13 +2,13 @@ import pickle
 import pandas as pd
 
 # Load trained SVD model
-with open("../models/svd_model.pkl", "rb") as f:
+with open("models/svd_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # Load ratings and movie titles
 column_names = ['user_id', 'item_id', 'rating', 'timestamp']
-ratings = pd.read_csv("../data/ml-100k/u.data", sep='\t', names=column_names)
-movies = pd.read_csv("../data/ml-100k/u.item", sep='|', encoding='latin-1', header=None)[[0, 1]]
+ratings = pd.read_csv("data/ml-100k/u.data", sep='\t', names=column_names)
+movies = pd.read_csv("data/ml-100k/u.item", sep='|', encoding='latin-1', header=None)[[0, 1]]
 movies.columns = ['item_id', 'title']
 
 # Merge to get full data with titles
